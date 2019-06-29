7d9b596ec320ffd6e12196272e7fefce9046a707
Italo <italoaurelior@gmail.com>
2019-06-29 06:39:15 -0300

    Tutorial part four Gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,61 @@
 import React from "react"
+import { graphql } from "gatsby"
+import { css } from "@emotion/core"
+import { rhythm } from "../utils/typography"
+import Layout from "../components/layout"
 
-export default () => <div>Hello world!</div>
+export default ({data}) => (
+  <Layout>
+    <h1>Amazing Pandas Eating Things</h1>
+    <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+        {data.allMarkdownRemark.edges.map(({ node }) => (
+          <div key={node.id}>
+            <h3
+              css={css`
+                margin-bottom: ${rhythm(1 / 4)};
+              `}
+            >
+              {node.frontmatter.title}{" "}
+              <span
+                css={css`
+                  color: #bbb;
+                `}
+              >
+                — {node.frontmatter.date}
+              </span>
+            </h3>
+            <p>{node.excerpt}</p>
+          </div>
+        ))}
+        Pages: {data.allMarkdownRemark.pageInfo.pageCount} 
+        Current Page: {data.allMarkdownRemark.pageInfo.currentPage}
+
+  </Layout>
+)
+
+export const query = graphql`
+  query {
+    allMarkdownRemark(sort: {order: DESC, fields: frontmatter___date}) {
+      edges {
+        node {
+          id
+          frontmatter {
+            date(formatString: "DD/MM/YYYY")
+            title
+          }
+          excerpt
+          html
+          timeToRead
+        }
+      }
+      pageInfo {
+        currentPage
+        hasNextPage
+        hasPreviousPage
+        itemCount
+        pageCount
+      }
+      totalCount
+    }
+  }
+`
